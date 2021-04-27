9dbf2192f817ae9adfed00fa5bf77245527d01d1
Priyansh Patel <43277093+priyanshpatel@users.noreply.github.com>
2021-04-27 01:02:56 -0700

    FE: RED-36

--- front-end/src/components/routes.js
+++ front-end/src/components/routes.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react'
 import { Route } from "react-router-dom"
 import Navbar from './navbar/navbar'
-
+import CreatePost from './posts/createPost';
 
 class Routes extends Component {
     render() {
         return (
             <div>
-                <Route path="/" component={Navbar} />
-
+                <Route exact path="/" component={Navbar} />
+                <Route  path="/submit/:id" component={CreatePost} />
 
 
 
