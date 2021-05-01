e6b89778e718706147a2a1b22c354424e2c48bd4
monilsakhidas <79077082+monilsakhidas@users.noreply.github.com>
2021-05-01 13:33:29 -0700

    BE: RED-40 & RED 41, Populated user fields

--- back-end/Utils/imageupload.js
+++ back-end/Utils/imageupload.js
@@ -1,29 +1,29 @@
 const path = require("path");
-import { S3 } from 'aws-sdk';
-import multer from 'multer';
-import multerS3 from 'multer-s3';
+import { S3 } from "aws-sdk";
+import multer from "multer";
+import multerS3 from "multer-s3";
 
 const s3 = new S3();
 
 const fileFilter = (req, file, cb) => {
-  if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png') {
+  if (file.mimetype === "image/jpeg" || file.mimetype === "image/png") {
     cb(null, true);
   } else {
-    cb(new Error('Invalid file type, only JPEG and PNG is allowed!'), false);
+    cb(new Error("Invalid file type, only JPEG and PNG is allowed!"), false);
   }
-}
+};
 
 export const uploadS3 = multer({
   fileFilter,
   storage: multerS3({
-    acl: 'public-read',
+    acl: "public-read",
     s3,
-    bucket: 'reddit-images-s3/images',
+    bucket: "reddit-images-s3/images",
     metadata: function (req, file, cb) {
-      cb(null, { fieldName: 'RedditImages' });
+      cb(null, { fieldName: "RedditImages" });
     },
     key: function (req, file, cb) {
-      cb(null, Date.now().toString())
-    }
-  })
+      cb(null, Date.now().toString());
+    },
+  }),
 });
